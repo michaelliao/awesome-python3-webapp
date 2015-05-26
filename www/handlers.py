de97357f080517ffb42d12aef360d733e2cd24f7
Michael Liao <askxuefeng@gmail.com>
2015-05-26 22:15:01 +0800

    merge day-08

--- www/handlers.py
+++ www/handlers.py
@@ -59,10 +59,16 @@ def cookie2user(cookie_str):
 
 @get('/')
 def index(request):
-    user = request.__user__
-    if user:
-        return 'Hello, %s!' % user.name
-    return 'Index Page'
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
+    return {
+        '__template__': 'blogs.html',
+        'blogs': blogs
+    }
 
 @get('/register')
 def register():
