493ce134ba781a621e15dd72b8592b12230f7d1f
Michael Liao <askxuefeng@gmail.com>
2015-05-26 22:10:49 +0800

    add mvc

--- www/handlers.py
+++ www/handlers.py
@@ -13,14 +13,13 @@ from models import User, Comment, Blog, next_id
 
 @get('/')
 def index(request):
-    user = request.__user__
-    if user:
-        return 'Hello, %s!' % user.name
-    return 'Index Page'
-
-@get('/api/users')
-def api_get_users():
-    users = yield from User.findAll(orderBy='created_at desc')
-    for u in users:
-        u.passwd = '******'
-    return dict(users=users)
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
+    return {
+        '__template__': 'blogs.html',
+        'blogs': blogs
+    }
