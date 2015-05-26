27337c2a462ae0d9b457ba476d39e7e81ac1e6f8
Michael Liao <askxuefeng@gmail.com>
2015-05-26 22:46:57 +0800

    remove orm

--- www/app.py
+++ www/app.py
@@ -14,12 +14,13 @@ from datetime import datetime
 
 from aiohttp import web
 
-import orm
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>')
 
 @asyncio.coroutine
 def init(loop):
-    yield from orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='www', password='www', db='awesome')
     app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
     srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
