46b87d436921e9128d8134517af8eebe1549d7cc
Michael Liao <askxuefeng@gmail.com>
2015-05-26 21:31:11 +0800

    remove blog list

--- www/handlers.py
+++ www/handlers.py
@@ -140,13 +140,6 @@ def signout(request):
     logging.info('user signed out.')
     return r
 
-@get('/manage/blogs')
-def manage_blogs(*, page='1'):
-    return {
-        '__template__': 'manage_blogs.html',
-        'page_index': get_page_index(page)
-    }
-
 @get('/manage/blogs/create')
 def manage_create_blog():
     return {
@@ -180,16 +173,6 @@ def api_register_user(*, email, name, passwd):
     r.content_type = 'application/json'
     r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
     return r
-
-@get('/api/blogs')
-def api_blogs(*, page='1'):
-    page_index = get_page_index(page)
-    num = yield from Blog.findNumber('count(id)')
-    p = Page(num, page_index)
-    if num == 0:
-        return dict(page=p, blogs=())
-    blogs = yield from Blog.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
-    return dict(page=p, blogs=blogs)
 
 @get('/api/blogs/{id}')
 def api_get_blog(*, id):
