b06410520287b9b4a57ec024fedf35c8e401d7aa
Michael Liao <askxuefeng@gmail.com>
2015-11-03 10:03:50 +0800

    upgrade to async syntax

--- www/app.py
+++ www/app.py
@@ -17,11 +17,10 @@ from aiohttp import web
 def index(request):
     return web.Response(body=b'<h1>Awesome</h1>')
 
-@asyncio.coroutine
-def init(loop):
+async def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
-    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
